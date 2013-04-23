d6e4f3371d6c47a3ed1d0dff33c8586dfba0b2f5
Christophe Gosiau <christophe@tigron.be>
2013-04-23 12:30:38 +0000

    we don't use the package version anymore

--- webroot/handler.php
+++ webroot/handler.php
@@ -2,14 +2,10 @@
 /**
  * Initialize the application
  *
- * @package %%PACKAGE%%
  * @author Christophe Gosiau <christophe@tigron.be>
  * @author Gerry Demaret <gerry@tigron.be>
- * @version $Id$
  */
 
 require_once '../config/global.php';
 require_once LIB_PATH . '/base/Web/Handler.php';
 Web_Handler::Run();
-
-?>
