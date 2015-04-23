012edfc2d8c04e350ef553a590446f194eb25098
Gerry Demaret <gerry@tigron.be>
2015-04-23 16:58:42 +0200

    update on po creation script

--- util/generate/po.php
+++ util/generate/po.php
@@ -1,246 +1,173 @@
 <?php
 /**
- * This script will generate po files based on all strings
- * that needs to be translated from the templates
- *
- * @author Christophe Gosiau <christophe@tigron.be>
- * @author Gerry Demaret <gerry@tigron.be>
+ * This tool generates the .po files for each application.
+ * The resulting files can be found in $ROOT/po/
  */
 
 require_once dirname(__FILE__) . '/../../config/global.php';
 
-require_once LIB_PATH . '/model/Language.php';
-require_once LIB_PATH . '/base/Util.php';
-require_once LIB_PATH . '/base/Translation.php';
-require_once LIB_PATH . '/base/Web/Template.php';
-
-$template_directories = array('template');
-
 $config = Config::Get();
-Twig_Autoloader::register();
 
-// This is a dummy line. A language is required, otherwise rendering the template for cache will cause an exception
-Translation::configure(Language::get_by_id(1), 'admin');
+// Fetch paths for Applications
+$applications = Application::get_all();
 
-
-$applications_array = $config->applications;
-$applications = array();
-foreach ($applications_array as $application) {
-	if (is_array($application)) {
-		$applications[] = $application['name'];
-	} else {
-		$applications[] = $application;
-	}
-}
-
-$to_generate = array();
+$paths = array();
 foreach ($applications as $application) {
-	if (file_exists(ROOT_PATH . '/app/' . $application . '/template')) {
-		$to_generate[] = array(	'app_name'	=>	$application,
-								'app_path'	=>	ROOT_PATH . '/app/' . $application . '/template');
-	}
+	$paths[$application->name] = $application->path;
 }
-if (file_exists(STORE_PATH . '/pdf/template')) {
-	$to_generate[] = array(	'app_name' => 'pdf',
-							'app_path' => STORE_PATH . '/pdf/template');
-}
-if (file_exists(STORE_PATH . '/email/template')) {
-	$to_generate[] = array(	'app_name' => 'email',
-							'app_path' => STORE_PATH . '/email/template');
-}
-$applications = $to_generate;
 
-echo 'creating template cache' . "\n";
-foreach ($applications as $application) {
-	$application_path = str_replace('template', '', $application['app_path']);
-	$template = new Web_Template($application['app_name'], $application_path);
+// Manually add mails and PDF documents
+//$paths['email'] = STORE_PATH . '/email/template';
+//$paths['pdf'] = STORE_PATH . '/pdf/template';
 
-	$files = get_files_from_directory($application['app_path']);
-	foreach ($files as $file) {
-		$template_file = str_replace($application['app_path'] . '/' , '', $file);
-		echo $application_path . $template_file . "\n";
-
-		$template->render($template_file, false);
-	}
-}
-echo "\n\n";
-
-/**
- * Second: make a copy of every po file
- */
-echo 'copying all po files' . "\n";
-foreach ($applications as $application) {
-	$languages = Language::get_all();
-
-	foreach ($languages as $language) {
-		$config = Config::Get();
-		if ($config->base_language == $language->name_short) {
-			continue;
-		}
-		if (file_exists(PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '.po')) {
-			rename(PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '.po', PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '_def.po');
-		}
-	}
+// Translate all the applications
+foreach ($paths as $application => $directory) {
+	translate_application($application, $directory);
 }
 
 /**
- * Third: translate every application
- */
-echo 'translate applications' . "\n";
-foreach ($applications as $application) {
-	translate_application($application);
-}
-
-/**
- * Fourth: do a merge from each reference file
- */
-foreach ($applications as $application) {
-	$languages = Language::get_all();
-
-	foreach ($languages as $language) {
-		$config = Config::Get();
-		if ($config->base_language == $language->name_short) {
-			continue;
-		}
-
-		if (!file_exists(PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '_def.po')) {
-			continue;
-		}
-
-		Util::po_merge(PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '_def.po', PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '.po');
-		rename(PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '_def.po', PO_PATH . '/' . $language->name_short . '/' . $application['app_name'] . '.po');
-	}
-}
-
-/**
- * Function
+ * Translate an application
  *
- * Translate application: take all files from the cache and translate
- * them.
+ * @param string $application Name of the application
+ * @param string $directory Application path
  */
-function translate_application($application) {
-	if (is_dir(TMP_PATH . '/twig/' . $application['app_name'])) {
-		$files = scandir(TMP_PATH . '/twig/' . $application['app_name']);
+function translate_application($application, $directory) {
+	$config = Config::Get();
 
-		foreach ($files as $file) {
-			if ($file[0] == '.') {
-				continue;
-			}
+	echo 'translating ' . $application . ' (' . $directory . ')';
 
-			if (is_dir(TMP_PATH . '/twig/' . $application['app_name'] . '/' . $file)) {
-				translate_directory(TMP_PATH . '/twig/' . $application['app_name'] . '/' . $file, $application['app_name']);
-				continue;
-			}
-		}
+	// Fetch the templates in this directory
+	$templates = get_templates($directory);
+	$strings = array();
+
+	// Parse all the files we found
+	foreach ($templates as $template) {
+		$strings = array_merge($strings, get_strings($template));
 	}
-}
 
-/**
- * Get files from directory recursively
- */
-function get_files_from_directory($directory) {
-	$files = scandir($directory);
-	$return_list = array();
-
-	foreach ($files as $key => $file) {
-
-		if ($file[0] == '.') {
-			unset($files[$key]);
-			continue;
-		}
-
-		if (is_dir($directory . '/' . $file)) {
-			unset($files[$key]);
-			$return_list = array_merge($return_list, get_files_from_directory($directory . '/' . $file));
-			continue;
-		}
-		$return_list[] = $directory . '/' . $file;
-
-	}
-	return $return_list;
-}
-
-/**
- * Function
- *
- * Translate directory: take all files from a certain directory and translate
- * them.
- */
-function translate_directory($directory, $application) {
-	$files = scandir($directory);
-
-	foreach ($files as $file) {
-		if ($file[0] == '.') {
-			continue;
-		}
-
-		if (is_dir($directory . '/' . $file)) {
-			translate_directory($directory . '/' . $file, $application);
-			continue;
-		}
-
-		translate_file($directory . '/' . $file, $application);
-	}
-}
-
-/**
- * Function
- *
- * Translate file: translate a certain file
- */
-function translate_file($filename, $application) {
+	// Translate the strings
 	$languages = Language::get_all();
-
 	foreach ($languages as $language) {
-		$config = Config::Get();
-		if ($config->base_language == $language->name_short) {
+		// Don't create a .po file if it is our base_language
+		if ($language->name_short == $config->base_language) {
 			continue;
 		}
 
-		if (!file_exists(PO_PATH . '/' . $language->name_short . '/')) {
-			mkdir(PO_PATH . '/' . $language->name_short . '/', 0755, true);
+		echo ' ' . $language->name_short;
+
+		// If we already have a (partially) translated file, merge
+		if (file_exists(PO_PATH . '/' . $language->name_short . '/' . $application . '.po')) {
+			$translated = Util::po_load(PO_PATH . '/' . $language->name_short . '/' . $application . '.po');
+			$old_translated = Util::po_load(PO_PATH . '/' . $language->name_short . '.po');
+			$translated = array_merge($translated, $old_translated);
+		} else {
+			$translated = array();
 		}
 
-		if (!file_exists(PO_PATH . '/' . $language->name_short . '/' . $application . '.po')) {
-			touch(PO_PATH . '/' . $language->name_short . '/' . $application . '.po');
-		}
-
-		$content = file($filename);
-		$content = substr($content[2], 3, strlen($content[2])-6);
-		echo 'translating ' . $filename . ' to ' . $language->name_short . ' for ' . $application . ' containing ' . $content . "\n";
-
-		$content = file_get_contents($filename);
-		$content_orig = $content;
-		$strings = array();
-
-		while (strpos($content, 'Translation::translate_plural("') !== false) {
-			$content = substr($content, strpos($content, 'Translation::translate_plural("') + strlen('Translation::translate_plural("'));
-			$string = substr($content, 0, strpos($content, '", "'));
-			$strings[] = stripslashes(str_replace('\t', "\t", $string));
-			$content = substr($content, strpos($content, '", "') + strlen('", "'));
-			$string = substr($content, 0, strpos($content, '", '));
-			$strings[] = stripslashes(str_replace('\t', "\t", $string));
-			$content = substr($content, strpos($content, '", '));
-		}
-
-		$content = $content_orig;
-
-		while (strpos($content, 'Translation::translate("') !== false) {
-			$content = substr($content, strpos($content, 'Translation::translate("') + strlen('Translation::translate("'));
-			$string = substr($content, 0, strpos($content, '")'));
-			$content = substr($content, strpos($content, '")'));
-			$strings[] = stripslashes(str_replace('\t', "\t", $string));
-		}
-
-		$current_strings = Util::po_load(PO_PATH . '/' . $language->name_short . '/' . $application . '.po');
-		$untranslated = array();
-
+		// Create a new array with the merged translations
+		$new_po = array();
 		foreach ($strings as $string) {
-			$untranslated[$string] = '';
+			if (isset($translated[$string]) and $translated[$string] != '') {
+				$new_po[$string] = $translated[$string];
+			} else {
+				$new_po[$string] = '';
+			}
 		}
 
-		$strings = array_merge($current_strings, $untranslated);
-		asort($strings);
-		Util::po_save(PO_PATH . '/' . $language->name_short . '/' . $application . '.po', $strings);
+		// And save!
+		Util::po_save(PO_PATH . '/' . $language->name_short . '/' . $application . '.po', $application, $language, $new_po);
 	}
+
+	echo "\n";
+}
+
+/**
+ * Parse all translatable stings out of a file
+ *
+ * @param string $file The full path of the file to parse
+ */
+function get_strings($file) {
+	$content = file_get_contents($file);
+
+	/**
+	 * {t}string{/t}
+	 */
+	preg_match_all("/\{t\}(.*?)\{\/t\}/", $content, $matches);
+	$smarty_strings = $matches[1];
+
+	/**
+	 * {% trans "string" %}
+	 */
+	preg_match_all("/\{%(.*?)trans \"(.*?)\"(.*?)%\}/", $content, $matches);
+	$twig_strings = $matches[2];
+
+	/**
+	 * {% trans 'string' %}
+	 */
+	preg_match_all("/\{%(.*?)trans '(.*?)'(.*?)%\}/", $content, $matches);
+	$twig_strings2 = $matches[2];
+
+	/**
+	 * 'string'|trans
+	 */
+	preg_match_all("/'(.*?)'\|trans/", $content, $matches);
+	$twig_strings3 = $matches[1];
+
+	/**
+	 * "string"|trans
+	 */
+	preg_match_all("/\"(.*?)\"\|trans/", $content, $matches);
+	$twig_strings4 = $matches[1];
+
+	/**
+	 * {% trans %}string{% endtrans %}
+	 */
+	preg_match_all("/\{% trans %\}(.*?)\{% endtrans %\}/s", $content, $matches);
+	$twig_strings5 = $matches[1];
+
+	/**
+	 * Translation::translate('string')
+	 */
+	preg_match_all("/Translation\:\:translate\(\"(.*?)\"\)/", $content, $matches);
+	$module_strings = $matches[1];
+
+	return array_merge($twig_strings, $twig_strings2, $twig_strings3, $twig_strings4, $twig_strings5, $smarty_strings, $module_strings);
+}
+
+/**
+ * Find all template files in a given directory
+ *
+ * @param string $directory Directory to search for templates
+ */
+function get_templates($directory) {
+	// Get all files
+	$files = scandir($directory);
+
+	// Loop over all the files, recurse if it is a directory
+	$templates = array();
+	foreach ($files as $file) {
+		if ($file[0] == '.') {
+			continue;
+		}
+
+		// If it is a directory, recurse
+		if (is_dir($directory . '/' . $file)) {
+			$dir_templates = get_templates($directory . '/' . $file);
+			foreach ($dir_templates as $dir_template) {
+				$templates[] = $dir_template;
+			}
+			continue;
+		}
+
+		// If it is a file that we support, add it to the result
+		if (strpos($file, '.') !== false) {
+			$file_parts = explode('.', $file);
+			$extension = array_pop($file_parts);
+			if ($extension == 'twig' OR $extension == 'tpl' OR $extension == 'php') {
+				$templates[] = $directory . '/' . $file;
+			}
+		}
+	}
+
+	return $templates;
 }
