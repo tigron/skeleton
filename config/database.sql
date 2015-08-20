46204892f798b90b0dff30eaf4dbc6fc5f5e28ae
Gerry Demaret <gerry@tigron.be>
2015-08-20 12:08:58 +0200

    Unify the database schema, use standard lengths

--- config/database.sql
+++ config/database.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `language` (
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `name_local` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `name_short` varchar(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `name_ogone` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `name_ogone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   FULLTEXT KEY `name_short` (`name_short`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -23,9 +23,9 @@ CREATE TABLE IF NOT EXISTS `log` (
 
 CREATE TABLE IF NOT EXISTS `file` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `unique_name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `mime_type` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `unique_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mime_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `size` int(11) NOT NULL,
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -42,11 +42,11 @@ CREATE TABLE IF NOT EXISTS `picture` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `lastname` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `country_id` int(11) NOT NULL,
   `date_of_birth` date NOT NULL,
   `admin` tinyint(1) NOT NULL,
