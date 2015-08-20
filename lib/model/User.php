a86af613e4a4ce361e89ed52fe13895822fc0c8f
Gerry Demaret <gerry@tigron.be>
2015-08-20 12:27:27 +0200

    Automatically rehash a user's password if needed
    This seems like a good idea (famous last words). This will ensure
    password hashes always match the best practice at the time of the PHP
    release.

--- lib/model/User.php
+++ lib/model/User.php
@@ -142,6 +142,12 @@ class User {
 			return false;
 		}
 
+		// If we got here, we can assume the password is correct. If the password
+		// is still using a weak hash, we can rehash it.
+		if (password_needs_rehash($user->password, PASSWORD_DEFAULT)) {
+			$user->set_password($password);
+		}
+
 		return $user;
 	}
 
