d1e17efdf5839d7bcc4f2a317f89bad2c2be9a5a
Gerry Demaret <gerry@tigron.be>
2015-10-16 15:24:29 +0200

    module/index: remove useless examples

--- app/admin/module/index.php
+++ app/admin/module/index.php
@@ -32,22 +32,5 @@ class Web_Module_Index extends Module {
 	 * @access public
 	 */
 	public function display() {
-		$template = \Skeleton\Core\Web\Template::Get();
-
-		$options = [
-						['id' => 1, 'name' => 'option 1'],
-						['id' => 2, 'name' => 'option 2'],
-						['id' => 3, 'name' => 'option 3']
-		];
-		$template->assign('dummy_options', $options);
-	}
-
-	/**
-	 * Demo action with redirect
-	 *
-	 * @access public
-	 */
-	public function display_demo_redirect() {
-		Web_Session::redirect('/index?action=i_have_been_redirected');
 	}
 }
