1c3d952fd731570aeb8f2a1f1d775bab2c595630
Gerry Demaret <gerry@tigron.be>
2020-09-09 10:26:44 +0200

    Don't create tables which are handled by packages
    Some of the tables created here were being initialised by the relevant
    packages. Don't initialise them here.

--- migrations/20150922_203123_init.php
+++ migrations/20150922_203123_init.php
@@ -18,21 +18,6 @@ class Migration_20150922_203123_Init extends \Skeleton\Database\Migration {
 	 */
 	public function up() {
 		$db = Database::Get();
-		$db->query("
-			CREATE TABLE IF NOT EXISTS `language` (
-			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-			  `name_local` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-			  `name_short` varchar(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-			  `name_ogone` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-			  PRIMARY KEY (`id`),
-			  FULLTEXT KEY `name_short` (`name_short`)
-			) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-		", []);
-
-		$db->query("
-			INSERT INTO `language` VALUES (1,'English','English','en','en_US'),(2,'French','Français','fr','fr_FR'),(3,'Dutch','Nederlands','nl','nl_NL');
-		", []);
 
 		$db->query("
 			CREATE TABLE IF NOT EXISTS `log` (
@@ -49,29 +34,6 @@ class Migration_20150922_203123_Init extends \Skeleton\Database\Migration {
 		", []);
 
 		$db->query("
-			CREATE TABLE IF NOT EXISTS `file` (
-			  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-			  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-			  `unique_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-			  `mime_type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-			  `size` int(11) NOT NULL,
-			  `created` datetime NOT NULL,
-			  PRIMARY KEY (`id`)
-			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-		", []);
-
-		$db->query("
-			CREATE TABLE IF NOT EXISTS `picture` (
-			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `file_id` int(11) NOT NULL,
-			  `width` int(11) NOT NULL,
-			  `height` int(11) NOT NULL,
-			  PRIMARY KEY (`id`),
-			  KEY `file_id` (`file_id`)
-			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
-		", []);
-
-		$db->query("
 			CREATE TABLE IF NOT EXISTS `user` (
 			  `id` int(11) NOT NULL AUTO_INCREMENT,
 			  `firstname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -84,18 +46,6 @@ class Migration_20150922_203123_Init extends \Skeleton\Database\Migration {
 			  PRIMARY KEY (`id`)
 			) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 		", []);
-
-		$db->query("
-			ALTER TABLE  `file` ADD  `md5sum` VARCHAR( 32 ) NOT NULL AFTER  `name`;
-		", []);
-
-		$db->query("
-			ALTER TABLE  `file` DROP  `unique_name`;
-		", []);
-
-		$db->query("
-			ALTER TABLE  `file` ADD `expiration_date` datetime NULL AFTER `size`;
-		", []);
 	}
 
 	/**
@@ -103,7 +53,5 @@ class Migration_20150922_203123_Init extends \Skeleton\Database\Migration {
 	 *
 	 * @access public
 	 */
-	public function down() {
-
-	}
+	public function down() {}
 }
